b3aacb1ce1a6c1a662cad15f112931803503779b
Ramraj Chimouriya <rc02431618@student.ku.edu.np>
2019-12-26 09:52:34 +0545

    BinaryTreeSearch Abstract class

---
+++ binarytree.h
@@ -0,0 +1,12 @@
+#ifndef BINARYTREE_H
+#define BINARYTREE_H
+
+class BinarySearchTree
+{
+    public:
+    virtual void preorderTraversal()=0;
+    virtual void add(int data)=0;
+    virtual bool search (int data)=0;
+};
+
+#endif
